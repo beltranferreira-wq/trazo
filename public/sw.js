051e82f15e28318ac3d2266e9c881ef5901c0774
beltranferreira-wq <beltranferreira@live.com>
2026-08-01 05:41:20 -0300

    Refactor service worker event handling
    Refactor service worker install and activate events to simplify cache clearing and client notifications.

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,11 @@
-// Service Worker desactivado - limpia todo el caché anterior
-self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
-    .then(() => {
-      // Notificar a todos los clientes que recarguen
-      self.clients.matchAll().then(clients => {
-        clients.forEach(client => client.postMessage({ type: 'CACHE_CLEARED' }));
-      });
-    })
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.matchAll({ includeUncontrolled: true }))
+      .then(clients => clients.forEach(c => c.postMessage('RELOAD')))
+      .then(() => self.registration.unregister())
   );
 });
-// No interceptar ningún fetch - dejar pasar todo al servidor
+ 
